1c73601e30e835fad4dec7b5258c33d92222a512
Natan Hugo Carvalho Evangelista <natan.hugo.carvalho.evangelista@ccc.ufcg.edu.br>
2025-07-26 16:18:28 -0300

    Organiza o projeto

---
+++ app/Main.hs
@@ -0,0 +1,8 @@
+module Main (main) where
+
+import Walls
+
+main :: IO ()
+main = do
+     display
+     movePointer 0 10
---
+++ src/Walls.hs
@@ -0,0 +1,31 @@
+module Walls where
+import System.Console.ANSI
+import System.IO (stdout)
+
+height = 8
+width = 18
+
+walls = [(x, y) | x <- [0..width], y <- [0, height]] ++ [(x, y) | x <- [0,width], y <- [1..height - 1]] ++ [(x, y) | x <- [2, 4..width - 2], y <- [2, 4..height - 2]]
+
+wallSymboll = "#"
+
+takeX :: (Int, Int) -> Int
+takeX (x, y) = x
+
+takeY :: (Int, Int) -> Int
+takeY (x, y) = y
+
+movePointer :: Int -> Int -> IO()
+movePointer x y = setCursorPosition y x
+
+displayWalls :: [(Int, Int)] -> IO()
+displayWalls [] = return ()
+displayWalls (h:hs) = do
+    movePointer (takeX h) (takeY h)
+    putStr wallSymboll
+    displayWalls hs
+
+display :: IO()
+display = do
+    clearScreen
+    displayWalls walls
