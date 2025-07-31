b3c105ae8bd34ffd79e2d95f97d1817cd916db71
Matheus-Adiel <matheuus.adiiel@gmail.com>
2025-07-31 00:25:46 -0300

    fix: fixed some function definition and add some data structure

--- src/Walls.hs
+++ src/Walls.hs
@@ -1,31 +1,60 @@
 module Walls where
 import System.Console.ANSI
-import System.IO (stdout)
+import System.IO
 
-height = 8
-width = 18
+newtype Point = Point (Int, Int) deriving (Eq)
 
-walls = [(x, y) | x <- [0..width], y <- [0, height]] ++ [(x, y) | x <- [0,width], y <- [1..height - 1]] ++ [(x, y) | x <- [2, 4..width - 2], y <- [2, 4..height - 2]]
+data Map = Map { 
+        walls :: [Point],
+        player :: Point
+    }
 
-wallSymboll = "#"
+data GameConfigs = GameConfigs {
+        height :: Int,
+        width :: Int
+    }
 
-takeX :: (Int, Int) -> Int
-takeX (x, y) = x
+data Symboll = Wall | Box | Player 
 
-takeY :: (Int, Int) -> Int
-takeY (x, y) = y
+symbollToChar :: Symboll -> Char
+symbollToChar Wall   = '#'
+symbollToChar Box    = '*'
+symbollToChar Player = '@'
+
+createMap :: Int -> Int -> Point -> Map
+createMap height width point = Map walls point
+    where
+        walls = [createPoint x y | x <- [0..width], y <- [0, height]] ++ 
+                [createPoint x y | x <- [0,width], y <- [1..height - 1]] ++ 
+                [createPoint x y | x <- [2, 4..width - 2], y <- [2, 4..height - 2]]
+
+createPoint :: Int -> Int -> Point
+createPoint x y = (Point (x, y))
+
+takeX :: Point -> Int
+takeX (Point(x, y)) = x
+
+takeY :: Point -> Int
+takeY (Point(x, y)) = y
 
 movePointer :: Int -> Int -> IO()
 movePointer x y = setCursorPosition y x
 
-displayWalls :: [(Int, Int)] -> IO()
+displayWalls :: [Point] -> IO()
 displayWalls [] = return ()
 displayWalls (h:hs) = do
     movePointer (takeX h) (takeY h)
-    putStr wallSymboll
+    putChar (symbollToChar Wall)
     displayWalls hs
 
-display :: IO()
-display = do
+displayPlayer :: Point -> IO()
+displayPlayer point = do
+    movePointer (takeX point) (takeY point)
+    putChar (symbollToChar Player)
+
+display :: Map -> IO()
+display map = do
     clearScreen
-    displayWalls walls
+    displayWalls (walls map)
+    displayPlayer (player map)
+    hFlush stdout
