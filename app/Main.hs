3bf3dc3f56031aa831ffac7dc9ad8a7642530114
Natan Hugo Carvalho Evangelista <natan.hugo.carvalho.evangelista@ccc.ufcg.edu.br>
2025-08-01 16:04:36 -0300

    fix: resolving conflicts errors

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,9 @@ import Map
 import Menu
 import System.Exit
 import System.Console.ANSI
+import Timer
+import Data.IORef
+import Control.Concurrent (threadDelay)
 
 main :: IO ()
 main = do
@@ -19,12 +22,27 @@ main = do
     setSGR [Reset]
     if escolha == "1" then
        startGame
-    else exitSuccess
+    else do
+        clearScreen
+        hideCursor
+        setSGR [SetConsoleIntensity BoldIntensity, SetColor Foreground Vivid Yellow]
+        setCursorPosition (9 `div` 2 - 2) 0
+        putStrLn "Not in the mood to play?"
+        threadDelay 1000000
+        putStrLn "Come back soon and have fun!!\n"
+        threadDelay 500000
+        setSGR [Reset]
+        showCursor
+        exitSuccess
+        
 
 startGame :: IO ()
 startGame = do
+    hideCursor
+    tempoRef <- newIORef 5
+    let gameConfigs = GameConfigs 8 18
+    startTimer tempoRef
     hSetBuffering stdin NoBuffering
     hSetEcho stdin False
-    let gameConfigs = GameConfigs 8 18
     let initialMap = createMap (height gameConfigs) (width gameConfigs)
-    gameLoop initialMap
+    gameLoop initialMap gameConfigs tempoRef
