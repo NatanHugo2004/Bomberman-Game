70fd1584f3f411bab1ec6f7867b30b28cbfc1a0a
Natan Hugo Carvalho Evangelista <natan.hugo.carvalho.evangelista@ccc.ufcg.edu.br>
2025-08-08 18:25:55 -0300

    doc: bomb

--- src/Bomb.hs
+++ src/Bomb.hs
@@ -11,14 +11,17 @@ plantBomb position timer = Bomb {bombPosition = position,
                                  timer = timer
                                 }
 
+--Função que adiciona uma bomba em uma lista de bombas, recebendo uma bomba, uma lista de bombas, e retornando uma nova lista de bombas, feita a partir da lista de bombas recebidas + a bomba passada como parâmetro. A bomba só será adicionada caso a lista tenha menos que 3 bombas.
 addBomb :: Bomb -> [Bomb] -> [Bomb]
 addBomb newBomb bombs
     | length bombs < 3 = newBomb : bombs  
     | otherwise        = bombs
 
+-- Função responsável por atualizar a bomba, diminuindo seu tempo, recebendo uma bomba e retornando uma nova bomba com o campo timer decrementado.
 updateBomb:: Bomb -> Bomb
 updateBomb bomb = Bomb (bombPosition bomb) ((timer bomb) -1)
 
+-- Função responsável por explodir bombas de um mapa, recebendo um mapa, uma lista de bombaas, retornaando um novo mapa com as bombas explodidas e uma nova explosão criada.
 explodeBombs :: Map -> [Bomb] -> Map
 explodeBombs mapa [] = mapa
 explodeBombs mapa (b:bs) = explodeBombs mapaAtualizado bs
@@ -35,16 +38,20 @@ explodeBombs mapa (b:bs) = explodeBombs mapaAtualizado bs
     boxesRestantes = filter (`notElem` pontosAfetados) (boxes mapa)
     mapaAtualizado = mapa { boxes = boxesRestantes, explosions = explosion: explosions mapa}
 
+--Função responsável por criar uma nova explosão. Recebendo uma lista de pontos e retornadno uma explosão.
 createExplosion :: [Point] -> Explosion
 createExplosion points = Explosion (points) (1)
 
+--Função responsável por atualizar uma explosão, recebendo uma explosão e retornando uma nova explosão, com os mesmos campos da anterior, porém com o timer decrementado.
 updateExplosion :: Explosion -> Explosion
 updateExplosion explosion = Explosion (explosionPosition explosion) ((time explosion) -1)
 
+--Função responsável por retornar todos os pontos de explosão de uma lista de explosões. Recebe uma lista de explosões, uma lista de pontos e retorna uma lista de pontos, que representa os pontos afetados por explosões.
 getExplosionsPoints :: [Explosion] -> [Point] -> [Point]
 getExplosionsPoints [] points = points
 getExplosionsPoints (j:js) points = getExplosionsPoints js (points ++ (explosionPosition j))
 
+--Função responsável por verificar se um player está morto ou não, para isso, ele recebe um ponto do mapa(o ponto do player), uma lista de explosões, e verifica se o player está em algum ponto de explosão. 
 isDead :: Point -> [Explosion] -> Bool
 isDead playerPosition explosions = playerPosition `elem` (getExplosionsPoints explosions []) 
 
