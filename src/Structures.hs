729d93811b8397f0a9654c3d531a444e532a03d6
Matheus-Adiel <matheuus.adiiel@gmail.com>
2025-08-09 11:13:45 -0300

    adicionado condição de vitória por Artur lima

--- src/Structures.hs
+++ src/Structures.hs
@@ -6,7 +6,10 @@ data Map = Map { walls      :: [Point],
                  boxes      :: [Point],
                  player     :: Point,
                  bombs      :: [Bomb],
-                 explosions :: [Explosion] }
+                 explosions :: [Explosion],
+                 door       :: Point,
+                 key        :: Maybe Point,
+                 hasKey     :: Bool } 
 
 data GameConfigs = GameConfigs { height     :: Int,
                                  width      :: Int,
